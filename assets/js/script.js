77d512e66ac776fc3ed0a4fd32c784bc85dfb892
Git_HArt <ticklyfeet84@hotmail.com>
2023-03-03 14:55:52 +0000

    Add function structure to js

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrong(){
+
+}
+
+function displayAddtionQuestion() {
+
+}
+
+function displaySubtractQuestion(){
+
+}
+
+function displayMultiplyQuestion(){
+
+}
+
+function displayDivisionQuestion(){
+
+}
+
+
